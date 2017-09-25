23c0e4292cdd2a67163b607ef1423e443a3338e1
Tim <0xTim@users.noreply.github.com>
2017-09-25 23:54:03 +0100

    Bump Vapor OAuth dependency to Swift 4 version

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
     	.package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.2.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/brokenhandsio/vapor-oauth.git", .upToNextMajor(from: "0.5.0")),
+        .package(url: "https://github.com/brokenhandsio/vapor-oauth.git", .upToNextMajor(from: "0.6.0")),
     ],
     targets: [
         .target(name: "VaporOAuthFluent", dependencies: ["Vapor", "VaporOAuth", "FluentProvider"]),
